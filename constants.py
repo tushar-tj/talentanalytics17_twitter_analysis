42ef7acdf8b8baf8555746cebddb0bd6b884d536
tushar <tushar.joshi.89@gmail.com>
2017-03-08 14:28:44 +0530

    remove credentials
    removing credentials from constants file

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 #Variables that contains the user credentials to access Twitter API 
-consumer_key = "7Vyu0y1kk9oWjSKnzn10isw20"
-consumer_secret = "Lipylh27nESv2ERhIquOWW813ma6PkQ6mJtIGKSg2GAcOhPLdX"
-access_token = "172002242-mjuJKS7qrdl9rE8fcPM4qY5wpOkqxfeDe4kD0XnD"
-access_token_secret = "OlxwlXm3ewoq6E1CbwpxyglLCLwIVAYWWABxyxJPX208p"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
